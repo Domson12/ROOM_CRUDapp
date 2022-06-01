4a737234f3a4879041d45d8194353c78b40b731a
Domson12 <dominikmz12@op.pl>
2022-06-01 11:44:55 +0200

    first commit. ROOM database implemented

---
+++ app/src/main/java/eu/tuto/roomdemo/MainActivity.kt
@@ -0,0 +1,43 @@
+package eu.tuto.roomdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import eu.tuto.roomdemo.databinding.ActivityMainBinding
+import eu.tuto.roomdemo.db.Subscriber
+import eu.tuto.roomdemo.db.SubscriberDatabase
+import eu.tuto.roomdemo.db.SubscriberRepository
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var subscriberViewModel: SubscriberViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val dao = SubscriberDatabase.getInstance(application).subscriberDAO
+        val repository = SubscriberRepository(dao)
+        val factory = SubscriberViewModelFactory(repository)
+        subscriberViewModel = ViewModelProvider(this, factory)[SubscriberViewModel::class.java]
+        binding.myViewModel = subscriberViewModel
+        binding.lifecycleOwner = this
+       initRecyclerView()
+    }
+
+    private fun initRecyclerView(){
+        binding.subscriberRecyclerview.layoutManager = LinearLayoutManager(this)
+        displaySubscribersList()
+    }
+    private fun displaySubscribersList() {
+        subscriberViewModel.getSaveSubscribers().observe(this) {
+            Log.i("MyTag", it.toString())
+            binding.subscriberRecyclerview.adapter = MyRecyclerViewAdapter(it,{selectedItem:Subscriber->listItemClicked(selectedItem)})
+        }
+    }
+    private fun listItemClicked(subscriber: Subscriber){
+        Toast.makeText(this, "selected name is ${subscriber.name}", Toast.LENGTH_SHORT).show()
+    }
+}
---
+++ app/src/main/java/eu/tuto/roomdemo/SubscriberViewModel.kt
@@ -0,0 +1,57 @@
+package eu.tuto.roomdemo
+
+import androidx.lifecycle.*
+import eu.tuto.roomdemo.db.Subscriber
+import eu.tuto.roomdemo.db.SubscriberRepository
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+class SubscriberViewModel(private val repository: SubscriberRepository) : ViewModel() {
+
+    val inputName = MutableLiveData<String?>()
+
+    val inputEmail = MutableLiveData<String?>()
+
+    val saveOrUpdateButtonText = MutableLiveData<String>()
+
+    val clearAllOrDeleteButtonText = MutableLiveData<String>()
+
+    init {
+        saveOrUpdateButtonText.value = "Save"
+        clearAllOrDeleteButtonText.value = "Clear all"
+    }
+
+    fun saveOrUpdate() {
+        val name = inputName.value!!
+        val email = inputEmail.value!!
+        insert(Subscriber(0, name, email))
+        inputName.value = null
+        inputEmail.value = null
+    }
+
+    fun clearAllOrDelete() {
+        clearAll()
+    }
+
+    fun insert(subscriber: Subscriber) = viewModelScope.launch {
+        repository.insert(subscriber)
+    }
+
+    fun update(subscriber: Subscriber) = viewModelScope.launch {
+        repository.update(subscriber)
+    }
+
+    fun delete(subscriber: Subscriber) = viewModelScope.launch {
+        repository.delete(subscriber)
+    }
+
+    fun clearAll() = viewModelScope.launch {
+        repository.deleteAll()
+    }
+
+    fun getSaveSubscribers() = liveData {
+        repository.subscribers.collect {
+            emit(it)
+        }
+    }
+}
